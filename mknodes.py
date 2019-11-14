58e3c25041d164143cca35f6862415f12e7c1c27
0kd <tsuchiurax@gmail.com>
2019-11-14 19:18:38 +0900

    add test.png

--- mknodes.py
+++ mknodes.py
@@ -8,11 +8,24 @@ a = "nodes.txt"
 ar = open(a)
 
 arr = [ i.split() for i in ar.read().split('\n') if i != '' ]
+pos = {}
+ilst = []
 
 for i in arr:
-    G.add_node(i[0])
-    G.add_node(i[1])
     G.add_edge(i[0], i[1])
+    ilst.append(i[0])
+    ilst.append(i[1])
+
+ilsta = list(set(ilst))
+
+lisu =  sorted(ilsta, key=lambda x:int(x.split('.')[1]))
+print(lisu)
+
+for i in range(len(lisu)):
+    pos[lisu[i]] = (i, i)
+    G.add_node(i)
+
+
 
 nx.draw(G)
 plt.savefig("test.png")
